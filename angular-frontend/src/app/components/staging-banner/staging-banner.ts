8e2daea318def8053297aaab86660b8a647f7d3e
TSimonEvolveum <tomas.simon@evolveum.com>
2026-07-27 10:33:49 +0200

    download pop-up window with guide and more info

---
+++ angular-frontend/src/app/components/staging-banner/staging-banner.ts
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2010-2025 Evolveum and contributors
+ *
+ * Licensed under the EUPL-1.2 or later.
+ */
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-staging-banner',
+  standalone: true,
+  templateUrl: './staging-banner.html',
+  styleUrls: ['./staging-banner.scss']
+})
+export class StagingBanner {}
